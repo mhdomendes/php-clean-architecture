78fd423496d00d1e0aad4011e3fe221e35c3dbdc
MatheusMendes <matheusgbe.mendes@outlook.com>
2025-07-09 21:07:00 -0300

    Conhecendo o Sistema

---
+++ src/Email.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Alura\Arquitetura;
+
+use InvalidArgumentException;
+
+class Email
+{
+    private string $endereco;
+
+    private function __construct(string $endereco){
+        if(filter_var($endereco, FILTER_VALIDATE_EMAIL) === false){
+            throw new InvalidArgumentException(
+                'Endereço de e-mail inválido'
+            );
+        }
+
+        $this->endereco = $endereco;
+    }
+
+    public function __toString(): string
+    {
+        return $this->endereco;
+    }
+}
