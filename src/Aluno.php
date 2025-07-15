ac6032cb65307fca75ac97eb641bc98f7fe2c353
MatheusMendes <matheusgbe.mendes@outlook.com>
2025-07-14 21:15:19 -0300

    feat: Enriquecendo o Dominio

--- src/Aluno.php
+++ src/Aluno.php
@@ -7,4 +7,25 @@ class Aluno
     private CPF $cpf;
     private string $nome;
     private Email $email;
+    private array $telefones;
+
+    public static function comCpfNomeEmail(string $cpf, string $nome, string $email): self
+    {
+        return new Aluno(new Cpf($cpf), $nome, new Email($email));
+    }
+
+    public function __construct(Cpf $cpf, string $nome, Email  $email)
+    {
+        $this->cpf = $cpf;
+        $this->nome = $nome;
+        $this->email = $email;
+    }
+
+    public function adicionarTelefone(string $ddd, string $numero)
+    {
+        $this->telefones[] = new Telefone($ddd, $numero);
+        return $this;
+    }
+
+    
 }
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -31,6 +31,18 @@ class ComposerAutoloaderInit2acf34ed1e1f03660a7b21332a5908a7
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit2acf34ed1e1f03660a7b21332a5908a7::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
